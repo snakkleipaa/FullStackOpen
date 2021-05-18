7a2d9006049694e07f4b68e3ee067fcc50db1cb7
snakkleipaa <75498221+snakkleipaa@users.noreply.github.com>
2021-05-18 13:45:33 +0300

    Changes for exercises 3.9 -3.11
    Changes added to both phonebook (part2) and phonebook_backend (part3)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -2,11 +2,20 @@ const { request, response } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+
+
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use(cors())
 
 morgan.token('data', (req) => JSON.stringify(req.body))
-
-app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+
+
+
+
 
 
 let persons = [
@@ -99,7 +108,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
