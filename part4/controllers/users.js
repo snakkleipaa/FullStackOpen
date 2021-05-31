66862fcf5c19b0bb53d74ebab67188a136f4861c
snakkleipaa <75498221+snakkleipaa@users.noreply.github.com>
2021-05-31 15:28:25 +0300

    Add files via upload

---
+++ part4/controllers/users.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcryptjs')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User
+    .find({}).populate('blogs', { url: 1, title: 1, author: 1 })
+    response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+    const body = request.body
+
+    const saltRounds = 10
+    
+    if (!body.password || body.password.length < 3) {
+        return response.status(400).json({
+            error: 'password must be at least 3 characters long'
+        })
+    } else {
+        const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+        const user = new User({
+            username: body.username,
+            name: body.name,
+            passwordHash
+        })
+    
+        const savedUser = await user.save()
+    
+        response.json(savedUser)
+    }
+})
+
+module.exports = usersRouter
